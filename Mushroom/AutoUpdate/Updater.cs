ff7b399dd572ba8ccaf9b08039d1ce5db83a6878
AaronRobert <Robert.Aaron2017@outlook.com>
2018-04-05 21:16:52 +0800

    update

---
+++ Mushroom/AutoUpdate/Updater.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoUpdate
+{
+    public class Updater
+    {
+        public static bool CheckVersion(string url,string version)
+        {
+
+            return false;
+        }
+    }
+}
