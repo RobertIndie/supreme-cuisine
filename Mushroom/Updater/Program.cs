06216ae9c7aff1c0c02ba04b166529f0bd229b40
AaronRobert <Robert.Aaron2017@outlook.com>
2018-04-05 21:09:12 +0800

    add updater

---
+++ Mushroom/Updater/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Updater
+{
+    class Program
+    {
+        public static void Log(string message)
+        {
+            Console.WriteLine("[Log]" + message);
+        }
+        static void Main(string[] args)
+        {
+            Log("Start updater");
+        }
+    }
+}
